83f4e818cea247e6c0a184fdce81de3498ed08ad
Pierluigi97 <barsanofiopierluigimoretto@gmail.com>
2024-12-03 15:56:34 +0100

    Exercise completed

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-function calcolaPrezzoViaggio() {
-    // Chiediamo all'utente di inserire i dati
-    const chilometri = prompt("Quanti chilometri vuoi percorrere?");
-    const eta = prompt("Quanti anni hai?");
-  
-    // Convertiamo i valori in numeri
-    const chilometriNumerico = parseFloat(chilometri);
-    const etaNumerica = parseInt(eta);
-  
-    // Calcoliamo il prezzo base
-    const prezzoBase = chilometriNumerico * 0.21;
-  
-    // Calcoliamo lo sconto in base all'età
-    let sconto = 0;
-    if (etaNumerica < 18) {
-      sconto = prezzoBase * 0.2; // Sconto del 20%
-    } else if (etaNumerica >= 65) {
-      sconto = prezzoBase * 0.4; // Sconto del 40%
+/*
+Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+ Questo richiederà un minimo di ricerca.
+
+ logica:
+    1. Imposto i dati di partenza 
+    2. Salvataggio in due variabili il risultato dei promptsu età e km
+    3. Logica per il calcolo dello sconto e del messaggio di output
+    4. Stampo il messaggio
+*/
+
+// 1.
+function calcolaPrezzo() {
+   
+    const km = parseFloat(prompt("Quanti chilometri vuoi percorrere?"));
+    const eta = parseInt(prompt("Qual è la sua età?"));
+
+    const prezzoBase = km * 0.21;
+    console.log('Il prezzo del tuo biglietto è di € '+ prezzoBase);
+    
+    if (eta < 18) {
+      sconto = prezzoBase * 0.2; // Sconto del 20% per minorenni
+      const prezzoJunior = prezzoBase - sconto;
+      const prezzoJuniorFixed = prezzoJunior.toFixed(2);  
+      
+      console.log("Il prezzo finale del tuo biglietto è di " + prezzoJuniorFixed + "€, viene applicato lo sconto del 20% per i minorenni.");
+      
+    } else if (eta >= 65) {
+      sconto = prezzoBase * 0.4; // Sconto del 40% per over 65
+      const prezzoSenior = prezzoBase - sconto;
+      const prezzoSeniorFixed = prezzoSenior.toFixed(2);
+      console.log("Il prezzo finale del tuo biglietto è di " + prezzoSeniorFixed + "€, viene applicato lo sconto del 40% per gli over 65.");
     }
+   
+}
   
-    // Calcoliamo il prezzo finale
-    const prezzoFinale = prezzoBase - sconto;
-  
-    // Visualizziamo il risultato nella console
-    console.log("Il prezzo del tuo biglietto è di " + prezzoFinale.toFixed(2) + " €");
-  }
-  
-  // Chiamiamo la funzione per avviare il calcolo
-  calcolaPrezzoViaggio();
+ 
+calcolaPrezzo();
